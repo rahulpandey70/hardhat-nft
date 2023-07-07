9f1849acad0ce189946483b43a3aff0750a9b499
rahulpandey70 <rahulpandey9103@gmail.com>
2023-07-08 01:17:31 +0530

    mocks deploy

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,23 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
+const GAS_PRICE_LINK = 1e9 //1000000000
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected, Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        log("Mocks deployed...")
+        log("---------------------------------------------")
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
