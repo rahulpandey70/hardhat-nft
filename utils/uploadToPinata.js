6d17e9214ccc8a5eab8a28b7d172c26107131f7b
rahulpandey70 <rahulpandey9103@gmail.com>
2023-07-08 21:42:54 +0530

    store image to pinata

---
+++ utils/uploadToPinata.js
@@ -0,0 +1,39 @@
+const pinataSDK = require("@pinata/sdk")
+const path = require("path")
+const fs = require("fs")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
+    const fullImagePath = path.resolve(imagesFilePath)
+    const files = fs.readdirSync(fullImagePath)
+
+    let responses = []
+    console.log("Uploading to pinata...")
+    for (fileIndex in files) {
+        console.log(`Working on ${fileIndex}...`)
+        const readableStreamForFile = fs.createReadStream(`${fullImagePath}/${files[fileIndex]}`)
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFile)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
+}
+
+async function storeTokenUriMetadata(metadata) {
+    try {
+        const response = await pinata.pinJSONToIPFS(metadata)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+    return null
+}
+
+module.exports = { storeImages, storeTokenUriMetadata }
